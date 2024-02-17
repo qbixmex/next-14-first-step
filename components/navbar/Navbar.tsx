8fb87bb5e4ef0b7ed8a06712981176925500c612
Daniel González Briseño <qbixmex@gmail.com>
2024-02-16 19:29:01 -0800

    navbar-improvement

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -3,6 +3,13 @@
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import navLink from './Navbar.styles';
+import { HomeIcon } from '@primer/octicons-react';
+
+const navLinks = [
+  { path: '/about', text: 'About' },
+  { path: '/pricing', text: 'Pricing' },
+  { path: '/contact', text: 'Contact' },
+];
 
 const Navigation = () => {
 
@@ -14,21 +21,16 @@ const Navigation = () => {
         href="/"
         className={navLink.link + (pathname === '/' && navLink.active)}
       >
-        Home
+        <HomeIcon /> Home
       </Link>
       <section className="flex gap-4">
-        <Link
-          href="/about"
-          className={navLink.link + (pathname === '/about' && navLink.active)}
-        >About</Link>
-        <Link
-          href="/pricing"
-          className={ navLink.link + (pathname === '/pricing' && navLink.active)}
-        >Pricing</Link>
-        <Link
-          href="/contact"
-          className={navLink.link + (pathname === '/contact' && navLink.active)}
-        >Contact</Link>
+        {navLinks.map(({ path, text }) => (
+          <Link
+            key={path}
+            href={path}
+            className={navLink.link + (pathname === path && navLink.active)}
+          >{text}</Link>
+        ))}
       </section>
     </nav>
   );
